1d8da2a6e467609e8d8371f355e62a34d47c3404
dale8nson <daletristanhutchinson@gmail.com>
2024-04-03 08:05:18 +1100

    display 'WELCOME' on canvas

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import { useRef, useEffect } from 'react'
 import * as THREE from 'three'
 import { Canvas } from "@react-three/fiber"
+import { Html } from '@react-three/drei'
 import { vec3 } from '@/lib/utils'
 
 export default function Home() {
@@ -20,10 +21,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Canvas ref={canvasRef}>
         <ambientLight color={0xffffff} />
-        <mesh position={vec3(0, 0, 0)} scale={vec3(1, 1, 1)}>
+        <mesh className='main' position={vec3(0, 0, 0)} scale={vec3(1, 1, 1)}>
           <planeGeometry args={[16, 10]} />
-          <meshBasicMaterial color={0xee0000} />
+          <meshBasicMaterial color={0x000000} />
         </mesh>
+        <Html wrapperClass='main'>
+          <div className='flex-col align-middle text-white'>
+            <h1>WELCOME</h1>
+          </div>
+        </Html>
       </Canvas>
      </main>
   );
