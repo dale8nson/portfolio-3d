2e6fbe696299d1e21a4ed5ee07740f11eed37ec6
dale8nson <daletristanhutchinson@gmail.com>
2024-04-03 11:02:14 +1100

    changed welcome message to Text3d; added font; added environment map

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 'use client'
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import * as THREE from 'three'
 import { Canvas } from "@react-three/fiber"
-import { Html } from '@react-three/drei'
+import { Html, Text3D, useCubeTexture } from '@react-three/drei'
 import { vec3 } from '@/lib/utils'
 
 export default function Home() {
+
+  const [mainMenu, setMainMenu ] = useState(null)
+
+  const canvasRef = useRef(null)
+  const mainMenuRef = useRef(null)
+
+  const cubeMap = useCubeTexture(['sh_rt.png', 'sh_lf.png', 'sh_up.png', 'sh_dn.png', 'sh_bk.png', 'sh_ft.png'], {path:'/'})
 
   useEffect(() => {
     if(!canvasRef.current) return
@@ -13,23 +20,30 @@ export default function Home() {
     canvasRef.current.setAttribute('width', innerWidth)
     canvasRef.current.setAttribute('height', innerHeight)
 
+    setMainMenu(mainMenuRef.current)
 
-  })
+  },[])
 
-  const canvasRef = useRef(null)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-black ">
       <Canvas ref={canvasRef}>
-        <ambientLight color={0xffffff} />
-        <mesh className='main' position={vec3(-3.5, 0, 0)} scale={vec3(.1, .1, 1)}>
+        {/* <ambientLight color={0xffffff} /> */}
+        <directionalLight position={vec3(-5, 5, 10)} />
+        <pointLight position={vec3(-3.5, 2, 1)} />
+         <mesh className='main' ref={mainMenuRef} position={vec3(0, 0, 0)} scale={vec3(1, 1, 1)}>
           <planeGeometry args={[16, 10]} />
           <meshBasicMaterial color={0x000000} />
-        </mesh>
-        <Html wrapperClass='main'>
-          <div className='flex-col align-middle text-white'>
-            <h1>WELCOME</h1>
+          <Html wrapperClass='main' distanceFactor={10} portal={mainMenu} transform>
+          <div className='flex-col align-middle text-[#ee0000]'>
+            {/* <h1>WELCOME</h1> */}
           </div>
         </Html>
+        </mesh>
+        <Text3D position={vec3(-3.5, 0, 0)} font='/Kastellar_Regular.json' bevelEnabled bevelSegments={5} bevelSize={.0005} bevelThickness={0.001}>
+          WELCOME
+          <meshPhongMaterial envMap={cubeMap} attach='material-0' color={0xffffff} shininess={100} />
+          <meshPhongMaterial envMap={cubeMap} attach='material-1' color={0xffffff} shininess={100} />
+        </Text3D>
       </Canvas>
      </main>
   );
