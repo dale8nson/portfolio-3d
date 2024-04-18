60ab964049e997628898bcb11a8b2f1a14d442c1
Dale Hutchinson <daletristanhutchinson@gmail.com>
2024-04-18 17:38:46 +1000

    removed additional heading

--- app/data/form/page.js
+++ app/data/form/page.js
@@ -9,7 +9,6 @@ export default function Page() {
   return (
     <main className='flex-col bg-black w-full h-screen my-8 mx-0'>
       <div className='flex-col w-11/12 h-[95vh]  mx-auto my-auto p-4 border-solid border-black bg-white border-2 rounded-md'>
-        <h1 className='text-xl my-4 font-bold text-center'>Rates of COVID deaths among vaccinated and unvaccinated compared to vaccination rates in the NSW population</h1>
         <div className='flex justify-around gap-4 align-middle'>
           <DataEntry />
           {/* <DataTable /> */}
