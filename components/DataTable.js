34e39487e26b740294fc4390ded406d49a1a09a3
Dale Hutchinson <daletristanhutchinson@gmail.com>
2024-04-18 17:24:17 +1000

    fixes

--- components/DataTable.js
+++ components/DataTable.js
@@ -91,7 +91,7 @@ export const DataTable = () => {
     queryFn: async () => request('/api/graphql', query)
   })
 
-  console.log('DataTable graphql query data: ', data?.cases)
+  // console.log('DataTable graphql query data: ', data?.cases)
 
   let cases
   let formattedCases = []
@@ -135,104 +135,70 @@ export const DataTable = () => {
   //   return <div><h1 className='text-4xl text-black'>Loading...</h1></div>
   // }
 
-  // if (error) {
-  //   return <div className='h-screen flex justify-center mx-10 w-10/12 p-8 overflow-y-scroll flex-wrap text-wrap'><h1 className='text-2xl text-black'>Error loading data: {JSON.stringify(error.message)}</h1></div>
-  // }
-
-  // if(loading || isPending) return (
-  //   <div className="rounded-md border-solid mx-2 z-10 w-3/4 h-10/12 border-black border-2 relative">
-  //   <Table className='absolute left-0 top-0 h-[80vh] '>
-  //     <TableHeader className='top-0'>
-  //       {table.getHeaderGroups().map((headerGroup) => (
-  //         <TableRow key={headerGroup.id}>
-  //           {headerGroup.headers.map((header) => {
-  //             return (
-  //               <TableHead key={header.id}>
-  //                 {header.isPlaceholder
-  //                   ? null
-  //                   : flexRender(
-  //                     header.column.columnDef.header,
-  //                     header.getContext()
-  //                   )}
-  //               </TableHead>
-  //             )
-  //           })}
-  //         </TableRow>
-  //       ))}
-  //     </TableHeader>
-  //     <TableBody className=' top-[50px] overflow-y-scroll h-[80vh]' >
-  //     <TableSkeleton colSpan={columns.length} />
-  //     </TableBody>
-  //     </Table>
-  //   </div >
-  // )
+  if (error) {
+    return <div className='h-screen flex justify-center mx-10 w-10/12 p-8 overflow-y-scroll flex-wrap text-wrap'><h1 className='text-2xl text-black'>Error loading data: {JSON.stringify(error.message)}</h1></div>
+  }
 
   if (isPending) return <TableSkeleton />
   if (loading) return <TableSkeleton />
 
   return (
-    <div className='h-screen w-screen flex-col' >
-    <Table className='overflow-hidden'>
-      <div style={{ height: `${virtualizer.getTotalSize()}px` }} className=" relative z-0 rounded-md border-solid border-black border-2 ">
-      <div ref={parentRef} className='h-[650px] relative z-0 overflow-scroll'>
-        <TableHeader className='bg-white outline outline-2 outline-offset-[-1px] p-2 z-50 w-full h-20 sticky top-0 left-0' >
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id} className=' sticky z-30'>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead className='relative outline-black outline border-2  bg-white z-30' key={header.id} style={{ width: header.getSize() }}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody className='relative'  >
-          {
-            table.getRowModel().rows?.length ?
-              virtualizer.getVirtualItems().map((virtualRow, index) => {
-                const row = table.getRowModel().rows[virtualRow.index]
-                return (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && 'selected'}
-                    className='relative'
-                    style={{
-                      height: `${virtualRow.size}px`,
-                      transform: `translateY(${virtualRow.start - index * virtualRow.size
-                        }px)`
-                    }}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id} className='relative -z-10'>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </TableCell>
-                    ))}
+    <div className='h-[90%] w-screen p-2 flex-col  flex-col w-11/12 h-full mx-auto mb-4 border-solid border-black bg-white border-2 rounded-md overflow-scroll' >
+      <div  className=" relative z-0 h-full mb-4 pb-4 rounded-md border-solid border-black border-2 overflow-scroll ">
+        <Table ref={parentRef} style={{ height: `${virtualizer.getTotalSize()}px` }} className=''>
+          <TableHeader className='bg-white outline outline-2 outline-offset-[-1px] p-2 z-50 w-full h-20 top-8 left-0' >
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} className=' sticky z-30'>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead className='relative outline-black outline border-2  bg-white z-30' key={header.id} style={{ width: header.getSize() }}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody className='relative h-full'  >
+            {
+              table.getRowModel().rows?.length ?
+                virtualizer.getVirtualItems().map((virtualRow, index) => {
+                  const row = table.getRowModel().rows[virtualRow.index]
+                  return (
+                    <TableRow
+                      key={row.id}
+                      data-state={row.getIsSelected() && 'selected'}
+                      className='relative'
+                      style={{
+                        height: `${virtualRow.size}px`,
+                        transform: `translateY(${virtualRow.start - index * virtualRow.size
+                          }px)`
+                      }}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id} className='relative -z-10'>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  )
+                }
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={columns.length} className="h-24 text-center">
+                      No results.
+                    </TableCell>
                   </TableRow>
                 )
-              }
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={columns.length} className="h-24 text-center">
-                    No results.
-                  </TableCell>
-                </TableRow>
-              )
-          }
-        </TableBody>
+            }
+          </TableBody>
+        </Table >
       </div>
     </div>
-      </Table >
-
-
-</div>
-
-
   )
 }
