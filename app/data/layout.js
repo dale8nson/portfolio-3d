a8d82dc773c5152c526fcc2d1d1ad778896950ab
Dale Hutchinson <daletristanhutchinson@gmail.com>
2024-04-18 17:40:48 +1000

    centred heading

--- app/data/layout.js
+++ app/data/layout.js
@@ -5,15 +5,13 @@ export default function Layout({ children }) {
 
   return (
     <div className='h-screen w-screen flex-col align-middle mx-auto'>
-      <div className='flex justify-evenly align-middle' >
-        <div className='flex align-baseline' >
-          <Button><Link href='/data/form'>FORM</Link></Button>
-          <Button><Link href='/data/table'>TABLE</Link></Button>
-          <Button><Link href='/home'>HOME</Link></Button>
-        </div>
-        <div className='text-white text-xl font-bold leading-2 my-auto'>
-          <h1>Rates of COVID deaths among vaccinated and unvaccinated compared to vaccination rates in the NSW population</h1>
-        </div>
+      <div className='flex align-baseline' >
+        <Button><Link href='/data/form'>FORM</Link></Button>
+        <Button><Link href='/data/table'>TABLE</Link></Button>
+        <Button><Link href='/home'>HOME</Link></Button>
+      </div>
+      <div className='flex text-white text-xl font-bold leading-2 my-4 justify-center mx-auto w-full'>
+        <h1>Rates of COVID deaths among vaccinated and unvaccinated compared to vaccination rates in the NSW population</h1>
       </div>
       {children}
     </div >
